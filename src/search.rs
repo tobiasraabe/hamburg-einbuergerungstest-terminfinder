078d95896acb783bde45f5d1f36958af9713cdb5
Tobias Raabe <raabe@posteo.de>
2024-06-29 01:08:39 +0200

    Parse more course info. (#12)

--- src/search.rs
+++ src/search.rs
@@ -1,39 +1,61 @@
 use indicatif::ProgressBar;
+use reqwest::blocking::Client;
+use reqwest::Url;
 use scraper::{Html, Selector};
+use std::error::Error;
 use std::process;
 use std::thread;
 use std::time::Duration;
 
+#[derive(Debug)]
 struct Appointment {
     is_full: bool,
+    course_id: String,
 }
 
+/// Searches for available appointments and prints them.
+///
+/// This function fetches documents containing appointment information in a loop.
+/// It parses the appointments from the documents and filters out the full ones.
+/// The number of available courses is then printed.
+/// If there are available courses, the function finishes the progress bar,
+/// prints a message, and attempts to open the website.
+///
+/// # Panics
+///
+/// This function will panic if there is an error fetching or parsing the appointments.
 pub fn search() {
-    let url = "https://www.vhs-hamburg.de/deutsch/einbuergerungstest-1058";
-
-    // Create a spinner for waiting.
     let pb = ProgressBar::new_spinner();
     pb.enable_steady_tick(Duration::from_millis(100));
     pb.set_message("Fetching appointments...");
 
     loop {
-        let appointments = match get_appointments(url) {
-            Ok(appointments) => appointments,
-            Err(e) => {
-                eprintln!("Error finding appointments: {e}");
-                process::exit(1);
-            }
+        let documents = match get_documents() {
+            Ok(documents) => documents,
+            Err(error) => panic!("Error fetching appointments: {error:?}"),
         };
+        let appointments = parse_appointments(documents);
 
-        let appointment_available = appointments.iter().any(|app| !app.is_full);
+        let available_courses: Vec<_> = appointments.iter().filter(|app| !app.is_full).collect();
+        let n_available_courses = available_courses.len();
 
-        if appointment_available {
-            pb.finish_with_message(format!(
-                "Found {} appointments. Some are available! Opening the website...",
+        if n_available_courses > 0 {
+            pb.finish_and_clear();
+            println!(
+                "{n_available_courses} of {} courses are available! Opening the website...",
                 appointments.len()
-            ));
+            );
 
-            if webbrowser::open(url).is_err() {
+            let url = if n_available_courses == 1 {
+                format!(
+                    "https://www.vhs-hamburg.de/kurs/einburgerungstest/{}",
+                    available_courses[0].course_id
+                )
+            } else {
+                "https://www.vhs-hamburg.de/deutsch/einbuergerungstest-1058".to_string()
+            };
+
+            if webbrowser::open(&url).is_err() {
                 println!("Failed to open website. Please open {url} manually.");
             }
             println!("Exiting...");
@@ -54,48 +76,58 @@ pub fn search() {
     }
 }
 
-fn get_appointments(url: &str) -> Result<Vec<Appointment>, Box<dyn std::error::Error>> {
-    let res = reqwest::blocking::get(url)?;
-    let body = res.text()?;
-    let document = Html::parse_document(&body);
+fn get_documents() -> Result<Vec<String>, Box<dyn Error>> {
+    let base_url = "https://www.vhs-hamburg.de/deutsch/einbuergerungstest-1058";
+    let client = Client::new();
+    let mut documents = Vec::new();
+    let mut page = 1;
 
-    let selector = Selector::parse(".course-meta__status-content").unwrap();
+    loop {
+        let url = Url::parse_with_params(base_url, &[("p", page.to_string())])?;
 
-    let appointments = document
-        .select(&selector)
-        .map(|node| {
-            let is_full = node.text().collect::<String>().contains("voll");
-            Appointment { is_full }
-        })
-        .collect::<Vec<_>>();
-    Ok(appointments)
+        let response = client.get(url.clone()).send()?;
+
+        // Check if the request was redirected to the first page
+        if response.url() != &url {
+            break;
+        }
+
+        let text = response.text()?;
+        documents.push(text);
+
+        page += 1;
+    }
+
+    Ok(documents)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn parse_appointments(documents: Vec<String>) -> Vec<Appointment> {
+    let mut appointments = Vec::new();
 
-    #[test]
-    fn test_get_appointments() {
-        let mut server = mockito::Server::new();
-        server
-            .mock("GET", "/")
-            .with_status(200)
-            .with_body("<div class='course-meta__status-content'>voll</div>")
-            .create();
-        let appointments = get_appointments(&server.url());
-        assert!(appointments.unwrap()[0].is_full);
+    let course_card_selector =
+        Selector::parse("article.course-card").expect("Hardcoded selector is fine.");
+    let course_link_selector =
+        Selector::parse(".course-card__title a").expect("Hardcoded selector is fine.");
+    let course_status_selector =
+        Selector::parse(".course-meta__status--danger span").expect("Hardcoded selector is fine.");
+
+    for document in documents {
+        let html_doc = Html::parse_document(&document);
+
+        for course_card in html_doc.select(&course_card_selector) {
+            if let Some(course_link_element) = course_card.select(&course_link_selector).next() {
+                if let Some(href) = course_link_element.value().attr("href") {
+                    if let Some(course_id) = href.split('/').nth(3) {
+                        let is_full = course_card.select(&course_status_selector).next().is_some();
+                        appointments.push(Appointment {
+                            is_full,
+                            course_id: course_id.to_string(),
+                        });
+                    }
+                }
+            }
+        }
     }
 
-    #[test]
-    fn test_get_appointments_with_available_appointment() {
-        let mut server = mockito::Server::new();
-        server
-            .mock("GET", "/")
-            .with_status(200)
-            .with_body("<div class='course-meta__status-content'>verfügbar</div>")
-            .create();
-        let appointments = get_appointments(&server.url());
-        assert!(!appointments.unwrap()[0].is_full);
-    }
+    appointments
 }
